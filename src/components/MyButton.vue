91b29895254c79831611ac58caf42c8339ca1325
ErikssonF <eriksson.nf@gmail.com>
2022-09-13 14:06:55 +0200

    initial commit

---
+++ src/components/MyButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="handleButtonClicked">{{text}}</button>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    text: {
+        type: String,
+        default: "No text"
+    }
+})
+
+const emit = defineEmits(['buttonClicked'])
+
+const handleButtonClicked = () => {
+    emit('buttonClicked', 29)
+}
+
+</script>
