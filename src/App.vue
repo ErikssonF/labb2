b01a02553e0573e544e1ee9911367b7b4a1d7e71
ErikssonF <eriksson.nf@gmail.com>
2022-09-13 14:06:51 +0200

    labb2

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <input ref="input" id="input" type="text" v-model="userInput" />
+
+  <button v-on:click="addTask">
+    Lägg till
+  </button>
+
+  <ul>
+    <li v-for="(value, index) in tasks">
+      {{index}} {{ value }}
+    </li>
+  </ul>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { reactive } from 'vue'
+import MyButton from './components/MyButton.vue'
+
+const userInput = ref();
+
+const tasks = ref([]);
+
+
+const addTask = () => {
+  tasks.value.push(userInput.value);
+};
+
+</script>
